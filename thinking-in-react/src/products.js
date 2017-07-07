ed8816d4b440abda3831fe721b1766c221620dd8
vinay mishra <vmishra@grepruby.com>
2017-07-07 13:13:06 +0530

    seprated productsfrom index.js and put in products.js

---
+++ thinking-in-react/src/products.js
@@ -0,0 +1,25 @@
+var products = [
+  { category: 'Movies', price: '$85.00', stocked: true, name: 'Prince of Persia 2' },
+  { category: 'Movies', price: '$16.99', stocked: false, name: 'Fast and Furious x' },
+  { category: 'Movies', price: '$12.99', stocked: true, name: 'The Dark NIght' },
+  { category: 'Movies', price: '$19.99', stocked: false, name: 'Spiderman homecoming' },
+  { category: 'Movies', price: '$29.99', stocked: true, name: 'Avengers 3' },
+  { category: 'Movies', price: '$39.99', stocked: false, name: 'Inception' },
+  { category: 'Movies', price: '$66.99', stocked: false, name: 'Zero Dark Thirty' },
+  { category: 'Movies', price: '$79.79', stocked: true, name: 'Titanic' },
+  { category: 'Movies', price: '$99.99', stocked: true, name: 'Winter Soldier' },
+  { category: 'Movies', price: '$10.99', stocked: true, name: 'Mad max' },
+  { category: 'Movies', price: '$5.99', stocked: true, name: 'Baby Driver' },
+  { category: 'Movies', price: '$2.99', stocked: true, name: 'The Matrix' },
+  { category: 'Movies', price: '$1.99', stocked: false, name: 'The last AirBender' },
+  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+  { category: 'Electronics', price: '$1399.99', stocked: false, name: 'iPhone 9' },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
+  { category: 'Fashion', price: '$19.99', stocked: true, name: 'Taxido' },
+  { category: 'Fashion', price: '$199.99', stocked: false, name: 'Banarasi Saari' }
+];
+
+export default products;
